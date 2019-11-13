40a8d62b30c715cfce0ec97efcf0b60c38d3dc87
da-okazaki <da-okazaki@outlook.com>
2019-11-13 22:12:26 +0900

    initial commit GoogleMapsMock

---
+++ GoogleMapsMock/GoogleMapsMock/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GoogleMapsMock
+//
+//  Created by Daichi Okazaki on 2019/11/13.
+//  Copyright © 2019 Daichi Okazaki. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoogleMapsMock/Pods/Target Support Files/Pods-GoogleMapsMock-GoogleMapsMockUITests/Pods-GoogleMapsMock-GoogleMapsMockUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleMapsMock_GoogleMapsMockUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleMapsMock_GoogleMapsMockUITests
+@end
